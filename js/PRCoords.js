2fcacf99f269205ec10bb881b39f7b62410d1bd8
Mingye Wang <arthur200126@gmail.com>
2017-02-08 20:28:20 -0500

    zh: + url (wikipedia sync)

--- js/PRCoords.js
+++ js/PRCoords.js
@@ -1,9 +1,10 @@
 "use strict"
 // <nowiki>
 /**
- * People's Republic of (distorted) [[T:Coord|Coordinates]]
+ * People's Rectified [[T:Coord|Coordinates]]
  * @file Utils for inserting valid WGS-84 coords from GCJ-02/BD-09 input
  * @author User:Artoria2e5
+ * @url https://github.com/Artoria2e5/PRCoords
  * 
  * @see [[:en:GCJ-02]]
  * @see https://github.com/caijun/geoChina (GPLv3)
@@ -59,165 +60,164 @@ var EARTH_R = 6371000
 /// Distance for haversine method; suitable over short distances like
 /// conversion deviation checking
 function distance(a, b) {
-    function hav(θ) {
-        return (1 - Math.cos(θ)) / 2
-    }
-
-    var Δ = _coord_diff(a, b)
-    return 2 * EARTH_R * Math.asin(Math.sqrt(
-        hav(Δ.lat) +
-        Math.cos(a.lon) * Math.cos(b.lon) * hav(Δ.lon)
-    ))
+	function hav(θ) {
+		return (1 - Math.cos(θ))/2
+	}
+	
+	var Δ = _coord_diff(a, b)
+	return 2 * EARTH_R * Math.asin(Math.sqrt(
+		hav(Δ.lat) +
+		Math.cos(a.lon) * Math.cos(b.lon) * hav(Δ.lon)
+	))
 }
 
 function sanity_in_china_p(coords) {
-    return coords.lat >= 0.8293 && coords.lat <= 55.8271 &&
-        coords.lon >= 72.004 && coords.lon <= 137.8347
+	return coords.lat >= 0.8293 && coords.lat <= 55.8271 &&
+           coords.lon >= 72.004 && coords.lon <= 137.8347
 }
 
 function _coord_diff(a, b) {
-    return {
-        lat: a.lat - b.lat,
-        lon: a.lon - b.lon,
-    }
+	return {
+		lat: a.lat - b.lat,
+		lon: a.lon - b.lon,
+	}
 }
 
 function _coord_error(a, b) {
-    var diff = _coord_diff(a, b)
-    return Math.max(Math.abs(diff.lat), Math.abs(diff.lot))
+	var diff = _coord_diff(a, b)
+	return Math.max(Math.abs(diff.lat), Math.abs(diff.lot))
 }
 
 function wgs_gcj(wgs, checkChina = true) {
-    if (checkChina && !sanity_in_china_p(wgs)) {
-        console.warn(`Non-Chinese coords found, returning as-is: ` +
-            `(${wgs.lat}, ${wgs.lon})`)
-        return wgs
-    }
-
-    var x = wgs.lon - 105,
-        y = wgs.lat - 35
-
-    // These distortion functions accept (x = lon - 105, y = lat - 35).
-    // They return distortions in terms of arc lengths, in meters.
-    //
-    // In other words, you can pretty much figure out how much you will be off
-    // from WGS-84 just through evaulating them...
-    // 
-    // For example, at the (mapped) center of China (105E, 35N), you get a
-    // default deviation of <300, -100> meters.
-    var dLat_m = -100 + 2 * x + 3 * y + 0.2 * y * y + 0.1 * x * y +
-        0.2 * Math.sqrt(Math.abs(x)) + (
-            2 * Math.sin(x * 6 * Math.PI) + 2 * Math.sin(x * 2 * Math.PI) +
-            2 * Math.sin(y * Math.PI) + 4 * Math.sin(y / 3 * Math.PI) +
-            16 * Math.sin(y / 12 * Math.PI) + 32 * Math.sin(y / 30 * Math.PI)
+	if (checkChina && !sanity_in_china_p(wgs)) {
+		console.warn(`Non-Chinese coords found, returning as-is: ` +
+					 `(${wgs.lat}, ${wgs.lon})`)
+		return wgs
+	}
+	
+	var x = wgs.lon - 105, y = wgs.lat - 35
+	
+	// These distortion functions accept (x = lon - 105, y = lat - 35).
+	// They return distortions in terms of arc lengths, in meters.
+	//
+	// In other words, you can pretty much figure out how much you will be off
+	// from WGS-84 just through evaulating them...
+	// 
+	// For example, at the (mapped) center of China (105E, 35N), you get a
+	// default deviation of <300, -100> meters.
+	var dLat_m = -100 + 2 * x + 3 * y + 0.2 * y * y + 0.1 * x * y +
+		0.2 * Math.sqrt(Math.abs(x)) + (
+	        2 * Math.sin(x * 6 * Math.PI) + 2 * Math.sin(x * 2 * Math.PI) +
+	        2 * Math.sin(y * Math.PI) + 4 * Math.sin(y / 3 * Math.PI) +
+	        16 * Math.sin(y / 12 * Math.PI) + 32 * Math.sin(y / 30 * Math.PI)
         ) * 20 / 3
-    var dLon_m = 300 + x + 2 * y + 0.1 * x * x + 0.1 * x * y +
-        0.1 * Math.sqrt(Math.abs(x)) + (
-            2 * Math.sin(x * 6 * Math.PI) + 2 * Math.sin(x * 2 * Math.PI) +
-            2 * Math.sin(x * Math.PI) + 4 * Math.sin(x / 3 * Math.PI) +
-            15 * Math.sin(x / 12 * Math.PI) + 30 * Math.sin(x / 30 * Math.PI)
+	var dLon_m = 300 + x + 2 * y + 0.1 * x * x + 0.1 * x * y +
+		0.1 * Math.sqrt(Math.abs(x)) + (
+        	2 * Math.sin(x * 6 * Math.PI) + 2 * Math.sin(x * 2 * Math.PI) +
+	        2 * Math.sin(x * Math.PI) + 4 * Math.sin(x / 3 * Math.PI) +
+	        15 * Math.sin(x / 12 * Math.PI) + 30 * Math.sin(x / 30 * Math.PI)
         ) * 20 / 3
-
-
+    
+    
     var radLat = wgs.lat / 180 * Math.PI
     var magic = 1 - GCJ_EE * Math.pow(Math.sin(radLat), 2) // just a common expr
-
+    
     // [[:en:Latitude#Length_of_a_degree_of_latitude]]
     var lat_deg_arclen = (Math.PI / 180) * (GCJ_A * (1 - GCJ_EE)) * Math.pow(magic, 1.5)
-        // [[:en:Longitude#Length_of_a_degree_of_longitude]]
+    // [[:en:Longitude#Length_of_a_degree_of_longitude]]
     var lon_deg_arclen = (Math.PI / 180) * (GCJ_A * Math.cos(radLat) / Math.sqrt(magic))
-
+    
     // The screwers pack their deviations into degrees and disappear.
     // Note how they are mixing WGS-84 and Krasovsky 1940 ellipsoids here...
     return {
-        lat: wgs.lat + (dLat_m / lat_deg_arclen),
-        lon: wgs.lon + (dLon_m / lon_deg_arclen),
+    	lat: wgs.lat + (dLat_m / lat_deg_arclen),
+    	lon: wgs.lon + (dLon_m / lon_deg_arclen),
     }
 }
 
 // rev_transform_rough; accuracy ~2e-6 deg (meter-level)
 function gcj_wgs(gcj, checkChina = true) {
-    return _coord_diff(gcj, _coord_diff(wgs_gcj(gcj, checkChina), gcj))
+	return _coord_diff(gcj, _coord_diff(wgs_gcj(gcj, checkChina), gcj))
 }
 
 function gcj_bd(gcj, __dummy__ = false) {
-    var x = gcj.lon
-    var y = gcj.lat
-
-    // trivia: pycoordtrans actually describes how these values are calculated
-    var r = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * Math.PI * 3000 / 180)
-    var θ = Math.atan2(y, x) + 0.000003 * Math.cos(x * Math.PI * 3000 / 180)
-
-    // Hard-coded default deviations again!
-    return {
-        lat: r * Math.sin(θ) + BD_DLAT,
-        lon: r * Math.cos(θ) + BD_DLON,
-    }
+	var x = gcj.lon
+	var y = gcj.lat
+	
+	// trivia: pycoordtrans actually describes how these values are calculated
+	var r = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * Math.PI * 3000 / 180)
+	var θ = Math.atan2(y, x) + 0.000003 * Math.cos(x * Math.PI * 3000 / 180)
+	
+	// Hard-coded default deviations again!
+	return {
+		lat: r * Math.sin(θ) + BD_DLAT,
+		lon: r * Math.cos(θ) + BD_DLON,
+	}
 }
 
 // Yes, we can implement a "precise" one too.
 // accuracy ~1e-7 deg (decimeter-level; exceeds usual data accuracy)
 function bd_gcj(bd, __dummy__ = false) {
-    var x = bd.lon - BD_DLON
-    var y = bd.lat - BD_DLAT
-
-    // trivia: pycoordtrans actually describes how these values are calculated
-    var r = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * Math.PI * 3000 / 180)
-    var θ = Math.atan2(y, x) - 0.000003 * Math.cos(x * Math.PI * 3000 / 180)
-
-    return {
-        lat: r * Math.sin(θ),
-        lon: r * Math.cos(θ),
-    }
+	var x = bd.lon - BD_DLON
+	var y = bd.lat - BD_DLAT
+	
+	// trivia: pycoordtrans actually describes how these values are calculated
+	var r = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * Math.PI * 3000 / 180)
+	var θ = Math.atan2(y, x) - 0.000003 * Math.cos(x * Math.PI * 3000 / 180)
+	
+	return {
+		lat: r * Math.sin(θ),
+		lon: r * Math.cos(θ),
+	}
 }
 
 function bd_wgs(bd, checkChina = true) {
-    return gcj_wgs(bd_gcj(bd), checkChina)
+	return gcj_wgs(bd_gcj(bd), checkChina)
 }
 
 function wgs_bd(bd, checkChina = true) {
-    return gcj_bd(wgs_gcj(bd, checkChina))
+	return gcj_bd(wgs_gcj(bd, checkChina))
 }
 
 // generic "bored function" factory, Caijun 2014
 // gcj: 4 calls to wgs_gcj; ~0.1mm acc
 function __bored__(fwd, rev) {
-    return function rev_bored(bad, checkChina = true) {
-        var wgs = rev(bad)
-        var old = bad
-
-        // Wait till we hit fixed point or get bored
-        for (var i = 0; i < 10 && _coord_error(wgs, old) > PRC_EPS; i++) {
-            old = wgs
-            wgs = _coord_diff(wgs, _coord_diff(fwd(wgs, false), bad))
-        }
-
-        return wgs
-    }
+	return function rev_bored(bad, checkChina = true) {
+		var wgs = rev(bad)
+		var old = bad
+		
+		// Wait till we hit fixed point or get bored
+		for (var i = 0; i < 10 && _coord_error(wgs, old) > PRC_EPS; i++) {
+			old = wgs
+			wgs = _coord_diff(wgs, _coord_diff(fwd(wgs, false), bad))
+		}
+		
+		return wgs
+	}
 }
 
 var exports = {
-    distance: distance,
-    wgs_gcj: wgs_gcj,
-    gcj_wgs: gcj_wgs,
-    gcj_bd: gcj_bd,
-    bd_gcj: bd_gcj,
-    wgs_bd: wgs_bd,
-    bd_wgs: bd_wgs,
-
-    // Precise functions using caijun 2014 method
-    //
-    // Why "bored"? Because they usually exceed source data accuracy -- the
-    // original GCJ implementation contains noise from a linear-modulo PRNG,
-    // and Baidu seems to do similar things with their API too.
-    __bored__: __bored__,
-    gcj_wgs_bored: __bored__(wgs_gcj, gcj_wgs),
-    bd_gcj_bored: __bored__(gcj_bd, bd_gcj),
-    bd_wgs_bored: __bored__(wgs_bd, bd_wgs),
+	distance: distance,
+	wgs_gcj: wgs_gcj,
+	gcj_wgs: gcj_wgs,
+	gcj_bd: gcj_bd,
+	bd_gcj: bd_gcj,
+	wgs_bd: wgs_bd,
+	bd_wgs: bd_wgs,
+	
+	// Precise functions using caijun 2014 method
+	//
+	// Why "bored"? Because they usually exceed source data accuracy -- the
+	// original GCJ implementation contains noise from a linear-modulo PRNG,
+	// and Baidu seems to do similar things with their API too.
+	__bored__: __bored__,
+	gcj_wgs_bored: __bored__(wgs_gcj, gcj_wgs),
+	bd_gcj_bored: __bored__(gcj_bd, bd_gcj),
+	bd_wgs_bored: __bored__(wgs_bd, bd_wgs),
 }
 
 if (typeof module === "object" && exports) {
-    module.exports = exports
+	module.exports = exports
 } else if (typeof window !== "undefined") {
-    window.PRCoords = exports
+	window.PRCoords = exports
 }
