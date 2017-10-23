978f8486c0dc173d58acd2cc8faf7ec5713caa28
Dingyuan Wang <abcdoyle888@gmail.com>
2017-10-23 12:24:01 +0800

    pgsql: add (partial)

---
+++ pgsql/prcoords.sql
@@ -0,0 +1,53 @@
+CREATE OR REPLACE FUNCTION public.geodistance
+(alat double precision, alng double precision, blat double precision, blng double precision)
+RETURNS double precision AS $$
+SELECT 2 * 6371000 * asin(
+  sqrt(
+    sin(radians($3-$1)/2)^2 +
+    sin(radians($4-$2)/2)^2 * cos(radians($1)) * cos(radians($3))
+  )
+) AS distance;
+$$ LANGUAGE SQL IMMUTABLE COST 100;
+
+CREATE OR REPLACE FUNCTION public.wgs_gcj (wgs point) RETURNS point AS
+$$
+DECLARE
+  GCJ_A CONSTANT double precision := 6378245;
+  GCJ_EE CONSTANT double precision := 0.00669342162296594323;
+  x double precision;
+  y double precision;
+  dLat_m double precision;
+  dLon_m double precision;
+  radLat double precision;
+  magic double precision;
+  lat_deg_arclen double precision;
+  lon_deg_arclen double precision;
+BEGIN
+  x := wgs[1] - 105;
+  y := wgs[0] - 35;
+  dLat_m := (-100 + 2 * x + 3 * y + 0.2 * y * y + 0.1 * x * y +
+    0.2 * sqrt(abs(x)) + (
+        2 * sin(x * 6 * pi()) + 2 * sin(x * 2 * pi()) +
+        2 * sin(y * pi()) + 4 * sin(y / 3 * pi()) +
+        16 * sin(y / 12 * pi()) + 32 * sin(y / 30 * pi())
+    ) * 20 / 3);
+  dLon_m := (300 + x + 2 * y + 0.1 * x * x + 0.1 * x * y +
+      0.1 * sqrt(abs(x)) + (
+          2 * sin(x * 6 * pi()) + 2 * sin(x * 2 * pi()) +
+          2 * sin(x * pi()) + 4 * sin(x / 3 * pi()) +
+          15 * sin(x / 12 * pi()) + 30 * sin(x / 30 * pi())
+      ) * 20 / 3);
+
+  radLat := radians(wgs[0]);
+  magic := 1 - GCJ_EE * power(sin(radLat), 2);
+
+  lat_deg_arclen := radians((GCJ_A * (1 - GCJ_EE)) * power(magic, 1.5));
+  lon_deg_arclen = radians(GCJ_A * cos(radLat) / sqrt(magic));
+
+  RETURN (wgs[0] + (dLat_m / lat_deg_arclen),
+          wgs[1] + (dLon_m / lon_deg_arclen));
+END;
+$$ LANGUAGE plpgsql IMMUTABLE COST 150;
+
+CREATE OR REPLACE FUNCTION public.gcj_wgs (gcj point) RETURNS point AS
+$$ SELECT $1 - (wgs_gcj($1) - $1) AS wgs $$ LANGUAGE SQL IMMUTABLE COST 150;
