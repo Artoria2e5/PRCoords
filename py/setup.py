3387594e0a8c124a44c36626ed912e0cadf6447a
Mingye Wang <arthur200126@gmail.com>
2019-08-16 10:48:43 +0800

    py: try less hard with readme (#9); bump 1.0.1
    the tarball does not include the parent dir, which contains the README.

--- py/setup.py
+++ py/setup.py
@@ -2,15 +2,21 @@
 from __future__ import with_statement
 from setuptools import setup
 
-with open("../README.md", "r") as f:
-    long_description = f.read()
+description="Public Domain library for rectifying Chinese coordinates (gcj-02/bd-09)"
+
+try:
+    with open("../README.md", "r") as f:
+        long_description = f.read()
+except:
+    # Will only happen with non-wheel archives and never on pypi
+    long_description = description
 
 setup(
     name="prcoords",
-    version="1.0.0",
+    version="1.0.1",
     author="Mingye Wang",
     author_email="arthur200126@gmail.com",
-    description="Public Domain library for rectifying Chinese coordinates (gcj-02/bd-09)",
+    description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Artoria2e5/PRCoords",
