388ea2286b52330c684110d022b49eccc94cdae1
Mingye Wang <arthur2e5@aosc.io>
2021-02-13 10:23:42 +0800

    approx: oops it's unusable

--- cpp/libprcoords.cc
+++ cpp/libprcoords.cc
@@ -63,15 +63,15 @@ typedef PRCoords (*PRCOp)(PRCoords);
 template<PRCOp fwd, PRCOp rev>
 PRCOORDS_LOCAL static PRCoords bored_reverse_conversion(PRCoords bad) {
 	assume_coord(bad);
-    PRCoords wgs = rev(bad);
-    PRCoords diff{INFINITY, INFINITY};
-    
-    int i = 0;
-    while ((fabs(diff.lat) + fabs(diff.lon)) > PRCOORDS_EPS && i++ < 10) {
-    	diff = fwd(wgs) - bad;
-        wgs = wgs - diff;
-    }
-    return wgs;
+	PRCoords wgs = rev(bad);
+	PRCoords diff{INFINITY, INFINITY};
+	
+	int i = 0;
+	while ((fabs(diff.lat) + fabs(diff.lon)) > PRCOORDS_EPS && i++ < 10) {
+		diff = fwd(wgs) - bad;
+		wgs = wgs - diff;
+	}
+	return wgs;
 }
 
 extern "C" {
@@ -87,39 +87,47 @@ PRCoords prcoords_wgs_gcj(PRCoords wgs) {
 	// 
 	// For example, at the (mapped) center of China (105E, 35N), you get a
 	// default deviation of <300, -100> meters.
-	PRCOORDS_NUM dLat_m = -100 + 2 * x + 3 * y + 0.2 * y * y + 0.1 * x * y +
-		0.2 * sqrt(fabs(x)) + (
-	        2 * sin(x * 6 * M_PI) + 2 * sin(x * 2 * M_PI) +
-	        2 * sin(y * M_PI) + 4 * sin(y / 3 * M_PI) +
-	        16 * sin(y / 12 * M_PI) + 32 * sin(y / 30 * M_PI)
-        ) * 20 / 3;
-	PRCOORDS_NUM dLon_m = 300 + x + 2 * y + 0.1 * x * x + 0.1 * x * y +
-		0.1 * sqrt(fabs(x)) + (
-        	2 * sin(x * 6 * M_PI) + 2 * sin(x * 2 * M_PI) +
-	        2 * sin(x * M_PI) + 4 * sin(x / 3 * M_PI) +
-	        15 * sin(x / 12 * M_PI) + 30 * sin(x / 30 * M_PI)
-        ) * 20 / 3;
-    
-    
-    PRCOORDS_NUM radLat = wgs.lat / 180 * M_PI;
-    PRCOORDS_NUM magic = 1 - GCJ_EE * pow(sin(radLat), 2); // just a common expr
-    
-    // [[:en:Latitude#Length_of_a_degree_of_latitude]]
-    PRCOORDS_NUM lat_deg_arclen = (M_PI / 180) * (GCJ_A * (1 - GCJ_EE)) / pow(magic, 1.5);
-    // [[:en:Longitude#Length_of_a_degree_of_longitude]]
-    PRCOORDS_NUM lon_deg_arclen = (M_PI / 180) * (GCJ_A * cos(radLat) / sqrt(magic));
-    
-    // The screwers pack their deviations into degrees and disappear.
-    // Note how they are mixing WGS-84 and Krasovsky 1940 ellipsoids here...
-    return PRCoords{
-    	wgs.lat + (dLat_m / lat_deg_arclen),
-    	wgs.lon + (dLon_m / lon_deg_arclen),
-    };
+#ifndef APPROX
+	PRCOORDS_NUM dLat_m = -100 + 2 * x + 3 * y + 0.2 * y * y + 0.1 * x * y
+		+ 0.2 * sqrt(fabs(x)) + (
+			2 * sin(x * 6 * M_PI) + 2 * sin(x * 2 * M_PI)
+			+ 2 * sin(y * M_PI) + 4 * sin(y / 3 * M_PI)
+			+ 16 * sin(y / 12 * M_PI) + 32 * sin(y / 30 * M_PI)
+		) * 20 / 3;
+	PRCOORDS_NUM dLon_m = 300 + x + 2 * y + 0.1 * x * x + 0.1 * x * y
+		+ 0.1 * sqrt(fabs(x)) + (
+			2 * sin(x * 6 * M_PI) + 2 * sin(x * 2 * M_PI)
+			+ 2 * sin(x * M_PI) + 4 * sin(x / 3 * M_PI)
+			+ 15 * sin(x / 12 * M_PI) + 30 * sin(x / 30 * M_PI)
+		) * 20 / 3;
+#else
+	// Approximation code from julia side. I should probably
+	// extract the xy-to-ne thing, but that would mean making the whole
+	// program templates. Not today.
+	// FIXME: The coefficients are giving me ridiculously large numbers!
+	PRCOORDS_NUM dLat_m = 0;
+	PRCOORDS_NUM dLon_m = 0;
+#endif
+
+	PRCOORDS_NUM radLat = wgs.lat / 180 * M_PI;
+	PRCOORDS_NUM magic = 1 - GCJ_EE * pow(sin(radLat), 2); // just a common expr
+
+	// [[:en:Latitude#Length_of_a_degree_of_latitude]]
+	PRCOORDS_NUM lat_deg_arclen = (M_PI / 180) * (GCJ_A * (1 - GCJ_EE)) / pow(magic, 1.5);
+	// [[:en:Longitude#Length_of_a_degree_of_longitude]]
+	PRCOORDS_NUM lon_deg_arclen = (M_PI / 180) * (GCJ_A * cos(radLat) / sqrt(magic));
+	
+	// The screwers pack their deviations into degrees and disappear.
+	// Note how they are mixing WGS-84 and Krasovsky 1940 ellipsoids here...
+	return PRCoords{
+		wgs.lat + (dLat_m / lat_deg_arclen),
+		wgs.lon + (dLon_m / lon_deg_arclen),
+	};
 }
 
 PRCoords prcoords_gcj_wgs(PRCoords gcj) {
 	assume_coord(gcj);
-    return gcj - (prcoords_wgs_gcj(gcj) - gcj);
+	return gcj - (prcoords_wgs_gcj(gcj) - gcj);
 }
 
 PRCoords prcoords_gcj_bd(PRCoords gcj) {
@@ -153,21 +161,21 @@ PRCoords prcoords_bd_gcj(PRCoords bd) {
 }
 
 PRCoords prcoords_wgs_bd(PRCoords wgs) {
-    return prcoords_gcj_bd(prcoords_wgs_gcj(wgs));
+	return prcoords_gcj_bd(prcoords_wgs_gcj(wgs));
 }
 
 PRCoords prcoords_bd_wgs(PRCoords bd) {
-    return prcoords_gcj_wgs(prcoords_bd_gcj(bd));
+	return prcoords_gcj_wgs(prcoords_bd_gcj(bd));
 }
 
 PRCoords prcoords_gcj_wgs_bored(PRCoords gcj) {
-    return bored_reverse_conversion<prcoords_wgs_gcj, prcoords_gcj_wgs>(gcj);
+	return bored_reverse_conversion<prcoords_wgs_gcj, prcoords_gcj_wgs>(gcj);
 }
 PRCoords prcoords_bd_gcj_bored(PRCoords bd) {
-    return bored_reverse_conversion<prcoords_gcj_bd, prcoords_bd_gcj>(bd);
+	return bored_reverse_conversion<prcoords_gcj_bd, prcoords_bd_gcj>(bd);
 }
 PRCoords prcoords_bd_wgs_bored(PRCoords bd) {
-    return bored_reverse_conversion<prcoords_wgs_bd, prcoords_bd_wgs>(bd);
+	return bored_reverse_conversion<prcoords_wgs_bd, prcoords_bd_wgs>(bd);
 }
 }
 
@@ -176,13 +184,13 @@ PRCoords prcoords_bd_wgs_bored(PRCoords bd) {
 #include <type_traits>
 
 std::string show_coord(PRCoords v) {
-    return std::to_string(v.lat) + ", " + std::to_string(v.lon);
+	return std::to_string(v.lat) + ", " + std::to_string(v.lon);
 }
 int main(void) {
-    std::cout << std::is_pod<PRCoords>::value << std::endl
-              << show_coord(prcoords_wgs_gcj(PRCoords{35, 105})) << std::endl
-              << show_coord(prcoords_wgs_bd(PRCoords{35, 105})) << std::endl
-	          << show_coord(prcoords_wgs_gcj(PRCoords{34, 106})) << std::endl
-	          << show_coord(prcoords_wgs_bd(PRCoords{34, 106})) << std::endl;
+	std::cout << std::is_pod<PRCoords>::value << std::endl
+			  << show_coord(prcoords_wgs_gcj(PRCoords{35, 105})) << std::endl
+			  << show_coord(prcoords_wgs_bd(PRCoords{35, 105})) << std::endl
+			  << show_coord(prcoords_wgs_gcj(PRCoords{34, 106})) << std::endl
+			  << show_coord(prcoords_wgs_bd(PRCoords{34, 106})) << std::endl;
 }
 #endif
